ea4011bcafe68155232e660654ccc82671aec33c
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-01-13 09:23:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Print Anagrams Together/print-anagrams-together.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+
+        dict1 = {}
+        for i in words:
+            group = ''.join(sorted(i))
+            if group not in dict1:
+                dict1[group] = [i]
+            else:
+                dict1[group].append(i)
+        return dict1.values()
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
