1fba20c39fdc9f4acbae2be0bc950131a92cdb59
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-01-16 19:45:17 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Rotating an Array/rotating-an-array.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def leftRotate(self, arr, n, d):
+        arr[:]=arr[d:]+arr[:d]
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        d = int(input())
+        ob = Solution()
+        ob.leftRotate(arr, n, d)
+        for xx in arr:
+            print(xx, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
