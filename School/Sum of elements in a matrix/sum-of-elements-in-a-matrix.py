92ba949352aaee63f1bfe1477185bfa5741f78fa
Shilpi Bouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-02-15 11:23:28 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of elements in a matrix/sum-of-elements-in-a-matrix.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def sumOfMatrix(self,N,M,Grid):
+        sum= 0
+        for i in range (N):
+            total=0
+            for j in range (M):
+                total +=Grid[i][j]
+            sum+=total
+        return sum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        N,M=map(int,input().strip().split(' '))
+        Grid = [[0 for i in range(M)] for j in range(N)]
+        for i in range(N):
+            s=list(map(int,input().strip().split(' ')))
+            for j in range(M):
+                Grid[i][j]=s[j]
+        ob=Solution()
+        print(ob.sumOfMatrix(N,M,Grid)) 
+# } Driver Code Ends
