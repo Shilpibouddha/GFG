4b41d94ad7236a61b78d594b7499c7bcf4177c04
Shilpi Bouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-04-08 00:22:52 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Factorial/factorial.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+
+class Solution:
+    def factorial (self, N):
+        if N <=1:
+            return 1
+        else:
+            return N * self.factorial(N-1)
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        ob = Solution()
+        print(ob.factorial(N))
+
+# } Driver Code Ends
